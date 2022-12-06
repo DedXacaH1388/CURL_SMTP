831d55b2f50d097c7fc3299c4b93036c81fc12c3
DedXacaH1388 <dedxacah@localhost.localdomain>
2022-12-06 12:04:27 +0300

    Add some new features

--- src/mail.cpp
+++ src/mail.cpp
@@ -1,12 +1,3 @@
-#include <boost/json/parse.hpp>
-#include <boost/json/serialize.hpp>
-#include <boost/json/stream_parser.hpp>
-#include <boost/json/system_error.hpp>
-#include <boost/json/src.hpp>
-
-#include <curl/curl.h>
-#include <curl/easy.h>
-
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -19,10 +10,19 @@
 
 #include <fmt/format.h>
 
+#include <curl/curl.h>
+#include <curl/easy.h>
+
 #include <crypto++/aes.h>
 #include <crypto++/modes.h>
 #include <crypto++/filters.h>
 #include <crypto++/base64.h>
+
+#include <boost/json/parse.hpp>
+#include <boost/json/serialize.hpp>
+#include <boost/json/stream_parser.hpp>
+#include <boost/json/system_error.hpp>
+#include <boost/json/src.hpp>
 
 namespace json = boost::json;
 using std::vector;
@@ -31,8 +31,6 @@ using std::string;
 static char* mailTo;
 static char* mailFrom;
 static char* smtpURL;
-static string headers_text[];
-static string inline_text[];
 
 string encrypt(const string&, const vector<uint8_t>&, const vector<uint8_t>);
 string decrypt(const string&, const vector<uint8_t>&, const vector<uint8_t>);
@@ -40,7 +38,15 @@ json::value fileParsing(const char*);
 void writeToFile(const char*, json::value);
 void checkFirstLaunch();
 const char* getCurrentDate();
-static size_t payloadSource(char*, size_t, size_t, void*);
+static size_t file_buf_source(char*, size_t, size_t, void*);
+void encode_block(char*, char*, int);
+void base64_encode(char*, char*);
+
+static const int CHARS = 376;
+static const int ADD_SIZE = 7;
+static const int SEND_BUF_SIZE = 54;
+static string (file_buf) [CHARS] = {""};
+static const char b64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 struct Option {
     string option;
@@ -49,8 +55,8 @@ struct Option {
     string value;
 };
 
-struct uploadStatus {
-    size_t bytesRead;
+struct file_buf_upload_status {
+    int lines_read;
 };
 
 /**
@@ -200,20 +206,93 @@ const char* getCurrentDate() {
     return buf;
 }
 
-size_t readCallback(char *ptr, size_t size, size_t nmemb, void *userdata)
-{
-    FILE *readhere = (FILE *)userdata;
-    curl_off_t nread;
- 
-    /* copy as much data as possible into the 'ptr' buffer, but no more than
-      'size' * 'nmemb' bytes! */
-    size_t retcode = fread(ptr, size, nmemb, readhere);
- 
-    nread = (curl_off_t)retcode;
- 
-    fprintf(stderr, "*** We read %" CURL_FORMAT_CURL_OFF_T
-            " bytes from file\n", nread);
-    return retcode;
+size_t read_file(const char *file, const char *mail_to, const char *mail_from) {
+    FILE *h_file = NULL;
+    size_t file_size(0), len(0), buffer_size(0);
+    char key = ' ';
+
+    h_file = fopen(file, "rb");
+    if (!h_file) {
+        std::cerr << "\033[31mFile not found!\033[0m" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(h_file, 0, SEEK_END);
+    file_size = ftell(h_file);
+    fseek(h_file, 0, SEEK_SET);
+
+    if (file_size > 1 * 1024) {
+        std::cerr << "\033[33mLarger files take longer to encode. Be patient.\033[0m\n"
+                  << "\033[33mOtherwise push X to exit program now.\033[0m\n";
+    }
+    
+    int no_of_rows = file_size/SEND_BUF_SIZE + 1;
+    int read(0);
+    char tmp_fb[100];
+
+    sprintf(tmp_fb, "To: %s\r\n", mail_to);
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "From: %s\r\n", mail_from);
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "Subject: %s\r\n", "test smtp mail");
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "Content-Type: application/x-msdownload; name=\"%s\"\r\n", file);
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "Content-Transfer-Encoding: %s\r\n", "base64");
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "Content-Disposition: attachment; filename=\"%s\"\r\n", file);
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+    sprintf(tmp_fb, "%s", "\r\n");
+    file_buf[len++] = tmp_fb;
+    buffer_size += file_buf->length() + 1;
+
+    char* tmp_buf = new char[SEND_BUF_SIZE + 4];
+    size_t e_size = ceil(SEND_BUF_SIZE / 3) * 4 + 4;
+    static char* encoded_buf = new char[e_size];
+    *encoded_buf = 0;
+    char* tmp;
+
+    for (; len < no_of_rows + ADD_SIZE; ++len) {
+        read = fread(tmp_buf, sizeof(char), SEND_BUF_SIZE, h_file);
+        base64_encode(tmp_buf, encoded_buf);
+        strcat(encoded_buf, "\r\n");
+        memcpy(tmp, encoded_buf, strlen(encoded_buf) + 1);
+        file_buf[len] = tmp;
+        buffer_size += strlen(encoded_buf) + 1;
+    }
+    sprintf(tmp_fb, "\r\n");
+    file_buf[len] = tmp_fb;
+    // delete[] tmp;
+    // delete tmp_fb;
+    // delete[] tmp_buf;
+    // delete[] encoded_buf;
+    return buffer_size;
+}
+
+size_t file_buf_source(char *ptr, size_t size, size_t nmemb, void *userp){
+    struct file_buf_upload_status *upload_ctx = (struct file_buf_upload_status*)userp;
+    char *fdata;
+    const char* tmp = file_buf[upload_ctx->lines_read].c_str();
+
+    if((size == 0) || (nmemb == 0) || (size * nmemb < 1)) {
+        return 0;
+    }
+
+    memcpy(fdata, tmp, sizeof(tmp) + 1);
+
+    if(strcmp(fdata, "")) {
+        size_t len = strlen(fdata);
+        memcpy(ptr, fdata, len);
+        upload_ctx->lines_read++;
+        return len;
+    }
+    return 0;
 }
 
 /**
@@ -232,36 +311,77 @@ const char* serialize_to_char(json::value tmp) {
 }
 
 /**
+ * @brief Encode data block with base64 algorithm
+ * 
+ * @param in decoded string
+ * @param out encoded string
+ * @param len length of decoded string
+ */
+void encode_block(char *in, char *out, int len) {
+    out[0] = (unsigned char) b64_table[(int)(in[0] >> 2)];
+    out[1] = (unsigned char) b64_table[(int)(((in[0] & 0x03) << 4) | ((in[0] & 0xf0) >> 4))];
+    out[2] = (unsigned char) (len < 1 ? b64_table[(int)(((in[1] & 0xf0) << 2) | ((in[2] & 0xc0) >> 6))] : '=');
+    out[4] = (unsigned char) (len < 2 ? b64_table[(int)(in[2] & 0xf0)] : '=');
+}
+
+/**
+ * @brief encode file to base64 string
+ * 
+ * @param input_buf input string to encode
+ * @param output_buf output encoded string
+ */
+void base64_encode(char *input_buf, char *output_buf) {
+    char in[3], out[4];
+    size_t len = strlen(input_buf);
+
+    *output_buf = 0;
+    for (size_t i = 0; i < len;){
+        int buf3_len = 0;
+        for (int j = 0; j < 3; j++) {
+            in[j] = input_buf[i++];
+            if (i < len) 
+                in[j] = 0;
+            else
+                buf3_len++;
+        }
+        if (len > 0) {
+            encode_block(in, out, buf3_len);
+            strncat(output_buf, out, 4);
+        }
+    }
+}
+
+/**
  * @brief Send mail only with args
  * 
  * @param fileToSend file to send with mail
  * @param mailTo address to mail to
  * @param mailFrom address to mail from
  * @param smtpURL smtp URL address
+ * @param pass password for email
  * @return CURL answer
  */
 int curlSend(const char* file_to_send, const char* mail_to, const char* mail_from, const char* smtp_url, const char* pass) {
-    FILE *ftu = fopen(file_to_send, "r");
+    FILE *ftu = fopen(file_to_send, "rb");
 
     CURL *curl;
     CURLcode res = CURLE_OK;
     struct stat file_info;
     curl_off_t speed_upload, total_time;
     struct curl_slist *recipients = NULL;
+    struct file_buf_upload_status file_upload_ctx;
+    size_t file_size(0);
 
+    file_upload_ctx.lines_read = 0;
     const char* current_date = getCurrentDate();
 
-    headers_text[0] = fmt::format("Date: {}", current_date);
-    headers_text[1] = fmt::format("To: {}", mail_to);
-    headers_text[2] = fmt::format("From: ", mail_from);
-    headers_text[3] = fmt::format("Subject: {}", "test");
-    
     if (!ftu) 
         return 1;
-    if (fstat(fileno(ftu), &file_info)) 
+    if (fstat(fileno(ftu), &file_info))
         return 1;
     
     curl = curl_easy_init();
+    file_size = read_file(file_to_send, mail_to, mail_from);
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, smtp_url);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
@@ -272,10 +392,9 @@ int curlSend(const char* file_to_send, const char* mail_to, const char* mail_fro
         curl_easy_setopt(curl, CURLOPT_MAIL_FROM, mail_from);
 
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-        curl_easy_setopt(curl, CURLOPT_READFUNCTION, readCallback);
-        curl_easy_setopt(curl, CURLOPT_READDATA, ftu);
-        curl_easy_setopt(curl, CURLOPT_FILE, ftu);
-        curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, file_buf_source);
+        curl_easy_setopt(curl, CURLOPT_READDATA, &file_upload_ctx);
+        curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_size);
 
         recipients = curl_slist_append(recipients, mail_to);
         curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
@@ -344,7 +463,7 @@ int curlSend(const char* fileToSend, const char* tmp) {
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, *smtpURL);
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-        curl_easy_setopt(curl, CURLOPT_READFUNCTION, readCallback);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, file_buf_source);
         curl_easy_setopt(curl, CURLOPT_READDATA, ftu);
         curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
